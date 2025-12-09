21c59d41e740e4a9bba26dd73333f9241789888d
LUCABOSS9000 <128054882+LUCABOSS9000@users.noreply.github.com>
2025-12-09 17:40:31 +0100

    Move Projects section right after About section

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import { LanguageProvider } from './contexts/LanguageContext';
 import Hero from './components/Hero';
-import Projects from './components/Projects';
 import About from './components/About';
+import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Education from './components/Education';
 import Skills from './components/Skills';
@@ -16,8 +16,8 @@ function App() {
       <div className="App">
         <Navigation />
         <Hero />
-        <Projects />
         <About />
+        <Projects />
         <Experience />
         <Education />
         <Skills />
